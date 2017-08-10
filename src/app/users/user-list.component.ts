cf36e23a854ed849fe1d0e0f5e2841d98c13f139
JPetrozzi <juanpablopetrozzi@gmail.com>
2017-08-10 14:13:49 -0300

    Delete de usuarios

--- src/app/users/user-list.component.ts
+++ src/app/users/user-list.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from "./user.service";
 import { User } from "./models/user";
 
+declare var jQuery: any;
+declare var $: any;
+
 @Component({
     selector: 'user-list',
     templateUrl: './user-list.component.html',
@@ -11,13 +14,16 @@ export class UserListComponent implements OnInit {
   public title: String;
   public users: Array<User>;
   public confirm_delete: string;
+  public user: any;
 
   constructor(private userService: UserService) {
     this.title = 'List of Users!';
     this.users = new Array<User>();
+    this.user = {};
   }
 
   ngOnInit() {
+    $('.modal').modal();
     this.loadUsers();
   }
 
@@ -32,6 +38,10 @@ export class UserListComponent implements OnInit {
         error => {console.error('An error occurred in dashboard component, navigating to login: ', error);}
       );
 
+  }
+
+  modalTrigger(user: User) {
+    this.user = user;
   }
 
   private loadUsers(): void {
